0610fa4105ada942f95ddb59b840fc2b1192a80a
mcastro-glitch <manuelcastrosepulveda02@gmail.com>
2025-07-20 15:30:28 -0400

    Signed-off-by: mcastro-glitch <manuelcastrosepulveda02@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,3 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  plugins: [vue()]
-})
-
-module.exports = {
- publicPath: process.env.NODE_ENV === 'production'
- ? '/EVAFINAL_FRONTEND/'
- : '/'
-}
